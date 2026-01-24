2116f843397173941a569262bcede9f0599633ff
MaAlami2020 <ma.alami.2020@alumnos.urjc.es>
2026-01-24 18:15:15 +0100

    commented fallback controller

--- store-gateway/src/main/java/com/webapp1a/gateway/controller/FallbackController.java
+++ store-gateway/src/main/java/com/webapp1a/gateway/controller/FallbackController.java
@@ -1,6 +1,6 @@
 package com.webapp1a.gateway.controller;
 
-import org.springframework.http.HttpStatus;
+/*import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,4 +32,4 @@ public class FallbackController {
     }
 
     
-}
+}*/
